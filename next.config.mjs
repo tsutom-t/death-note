4cc4c56f2fefeb70fa90ef4021073fd4ea547099
tsutom-t <takatop162@gmail.com>
2024-06-22 12:03:28 +0900

    fix(config):basepath

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: process.env.REPOSITORY_NAME,
+  basePath: "death-note",
   images: {
     unoptimized: true,
   },
